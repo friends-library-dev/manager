c1dc30e09af05f03232a38460f1f9afd6889ee85
Jared Henderson <jaredh159@users.noreply.github.com>
2022-03-04 12:40:15 -0500

    remove refs to deprecated friends lib

---
+++ no-indent.sh
@@ -0,0 +1,10 @@
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/sarah-lynes-grubb --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/mary-dudley --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/ann-branson --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/john-griffith/ancient-path --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/robert-barclay/waiting-upon-the-lord --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/james-nayler --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/john-gough/history-of-the-quakers-v2 --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/john-gough/history-of-the-quakers-v4 --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/thomas-scattergood --force
+node ~/fl/apps/cli/dist/app.js publish -d 9999 -p en/samuel-rundell/vital-religion/updated --force
